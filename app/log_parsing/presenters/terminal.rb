9c7f42fa07e0f4718cba259f32692bc266fe6890
Lyubov Onufriyuk <lika.lex555@gmail.com>
2020-08-29 15:35:27 +0300

    GREEN implement terminal presenter

--- app/log_parsing/presenters/terminal.rb
+++ app/log_parsing/presenters/terminal.rb
@@ -2,9 +2,14 @@ module LogParsing
   module Presenters
     class Terminal
       def present_statistics(statistics)
+        puts('Number of visitors:')
+        puts(::Terminal::Table.new(rows: statistics[:overall]))
+        puts('Number of unique visitors:')
+        puts(::Terminal::Table.new(rows: statistics[:uniq]))
       end
 
       def present_message(message)
+        puts(message)
       end
     end
   end
